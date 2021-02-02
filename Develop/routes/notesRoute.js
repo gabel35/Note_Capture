a6263c0d7a9251a99522ac5dbc9de275c0c90333
gabel35 <gabrieljose3135@gmail.com>
2021-02-02 17:07:32 -0500

    what's wrong?!

--- Develop/routes/notesRoute.js
+++ Develop/routes/notesRoute.js
@@ -8,16 +8,26 @@ module.exports = function(app) {
 
 module.exports = function(app) {
 
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
   app.get("/api/notes", (req, res) => {
     const file = fs.readFileSync(db, "utf8");
     res.json(JSON.parse(file));
   });
   
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-  app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
+  app.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  })
+
+  app.get("/api/notes/:id", function(req, res) {
+    let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNote[Number(req.params.id)]);
+})
   
 }
