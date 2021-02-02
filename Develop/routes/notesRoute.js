0eba06114edbb92e7c3a19e46d8b9294c00bc637
gabel35 <gabrieljose3135@gmail.com>
2021-02-02 00:30:55 -0500

    worked on the server more

--- Develop/routes/notesRoute.js
+++ Develop/routes/notesRoute.js
@@ -5,3 +5,40 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 }
+
+module.exports = function(app) {
+
+  app.get("/api/notes", (req, res) => {
+    const file = fs.readFileSync(db, "utf8");
+    res.json(JSON.parse(file));
+  });
+  
+  app.post("/api/notes", (req, res) => {
+    fs.readFile(db, (err, data) => {
+      if (err) throw err;
+      let newNote = { ...req.body, id: uuid() };
+      let notesArr = JSON.parse(data);
+      let updatedNotesArr = [...notesArr, newNote];
+  
+      fs.writeFileSync(db, JSON.stringify(updatedNotesArr));
+      res.json(newNote);
+    });
+  });
+  
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(db, "utf-8", (err, data) => {
+      if (err) throw err;
+      let id = req.params.id;
+      let notesArr = JSON.parse(data);
+  
+      let deleteNote = _.find(notesArr, (note) => note.id === id);
+  
+      let updatedNotesArr = notesArr.filter(
+        (note) => !_.isEqual(note, deleteNote)
+      );
+  
+      fs.writeFileSync(db, JSON.stringify(updatedNotesArr));
+      res.json(deleteNote);
+    });
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var fs = require("fs");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -8,6 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./Develop/routes/notesRoute.js")(app);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "develop/public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "develop/public/notes.html"));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
