d3268d4a0d3f3eabd6ef7092add6d4d8f87cb06c
gabel35 <gabrieljose3135@gmail.com>
2021-02-02 16:39:20 -0500

    connected routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var fs = require("fs");
+const db = "db/db.json";
+const path = require("path");
+
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -7,13 +10,18 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./Develop/routes/notesRoute.js")(app);
+// require("/routes/notesRoute.js")(app);
+
+app.get("/api/notes", (req, res) => {
+  const file = fs.readFileSync(db, "utf8");
+  res.json(JSON.parse(file));
+});
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, function() {
