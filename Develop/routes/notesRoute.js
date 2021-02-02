2a3dad1a99bf46a5de117d7d27f4bf795171b45a
gabel35 <gabrieljose3135@gmail.com>
2021-02-02 17:35:54 -0500

    fixing routes

--- Develop/routes/notesRoute.js
+++ Develop/routes/notesRoute.js
@@ -1,4 +1,9 @@
 var path = require("path");
+var express = require("express");
+var fs = require("fs");
+var db = "db/db.json";
+
+var app = express();
 
 module.exports = function(app) {
   app.get("/notes", function(req, res) {
@@ -11,7 +16,7 @@ module.exports = function(app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  
+
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
@@ -22,12 +27,41 @@ module.exports = function(app) {
   });
   
   app.get("/api/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "../db/db.json"));
   })
 
   app.get("/api/notes/:id", function(req, res) {
-    let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNote = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
     res.json(savedNote[Number(req.params.id)]);
-})
+  })
+
+  app.post("/api/notes", (req, res) => {
+    fs.readFile(db, (err, data) => {
+      if (err) throw err;
+      let newNote = { ...req.body, id: uuid() };
+      let notesArr = JSON.parse(data);
+      let updatedNotesArr = [...notesArr, newNote];
+
+      fs.writeFileSync(db, JSON.stringify(updatedNotesArr));
+      res.json(newNote);
+    });
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(db, "utf-8", (err, data) => {
+      if (err) throw err;
+      let id = req.params.id;
+      let notesArr = JSON.parse(data);
+  
+      let deleteNote = _.find(notesArr, (note) => note.id === id);
+  
+      let updatedNotesArr = notesArr.filter(
+        (note) => !_.isEqual(note, deleteNote)
+      );
+  
+      fs.writeFileSync(db, JSON.stringify(updatedNotesArr));
+      res.json(deleteNote);
+    });
+  });
   
 }
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var fs = require("fs");
-const db = "db/db.json";
-const path = require("path");
+var db = "db/db.json";
+var path = require("path");
 
 var app = express();
 
@@ -11,8 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/notesRoute.js")(app);
-
-
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
