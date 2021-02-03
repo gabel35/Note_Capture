ac74a6a18c078f80227fc1fe142b2fb1d2814ab7
gabel35 <gabrieljose3135@gmail.com>
2021-02-02 22:46:40 -0500

    thank you office hours

---
+++ routes/notesRoute.js
@@ -0,0 +1,66 @@
+var path = require("path");
+var express = require("express");
+var fs = require("fs");
+var db = "db/db.json";
+
+var app = express();
+
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+}
+
+module.exports = function(app) {
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  app.get("/api/notes", (req, res) => {
+    const file = fs.readFileSync(db, "utf8");
+    res.json(JSON.parse(file));
+  });
+  
+  app.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+  })
+
+  app.get("/api/notes/:id", function(req, res) {
+    let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(saveNotes[Number(req.params.id)]);
+  })
+
+  app.post("/api/notes", function(req, res) {
+    let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let note = req.body;
+    let id = (saveNotes.length).toString();
+    note.id = id;
+    saveNotes.push(note);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(saveNotes));
+    console.log(`Your note, ${note.title}, has been saved!`, note);
+    res.json(saveNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let saveNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`You deleted note, ${note.title}.`, note);
+    saveNotes = saveNotes.filter(currentNote => {
+        return currentNote.id != noteID;
+    })
+    for (currentNote of saveNotes) {
+        currentNote.id = newID.toString();
+        newID++;
+    }
+  fs.writeFileSync("./db/db.json", JSON.stringify(saveNotes));
+  res.json(saveNotes);
+})
+  
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var fs = require("fs");
-var db = "db/db.json";
+var db = "/db/db.json";
 var path = require("path");
 
 var app = express();
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/notesRoute.js")(app);
 
